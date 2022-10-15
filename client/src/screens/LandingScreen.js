8150425907fc16eed18fc96bca9de0e06b9ce07c
“Your <mudra.s@ahduni.edu.in>
2022-10-16 00:04:33 +0530

    Added CHanges Related to frontend

--- client/src/screens/LandingScreen.js
+++ client/src/screens/LandingScreen.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
+import images from './images';
+import Slider from 'react-touch-drag-slider';
+
 // ..
 AOS.init({
 	duration: 2000,
@@ -17,6 +20,23 @@ function LandingScreen() {
 				<h1 data-aos='zoom-out' style={{ color: 'ede9fe' }}>
 					Welcome to our Website!!
 				</h1>
+				<Slider
+					onSlideComplete={(i) => {
+						console.log('finished dragging, current slide is', i);
+					}}
+					onSlideStart={(i) => {
+						console.log('started dragging on slide', i);
+					}}
+					activeIndex={0}
+					threshHold={100}
+					transition={0.5}
+					scaleOnDrag={true}
+				>
+					{images.map(({ url, title }, index) => (
+						<img src={url} key={index} alt={title} />
+					))}
+				</Slider>
+
 				<Link to='/home'>
 					<button className='btn btn-primary landingBtn'>Lets Go !!</button>
 				</Link>
